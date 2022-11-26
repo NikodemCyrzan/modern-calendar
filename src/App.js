f0de60571f2ab460a23f72c6124ee1f93c32a779
NikodemCyrzan <nikodemcyrzan81@gmail.com>
2022-11-27 00:19:14 +0100

    feat: styled date input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import "./App.css";
+import "./App.scss";
+import calendarIcon from "./icons/calendar.svg";
 
 function App() {
-    return <div className="container">Test</div>;
+    return (
+        <div className="calendar">
+            <div className="calendar__wrapper">
+                <div className="calendar__container">
+                    <div className="calendar-input__container calendar-input__container--focused">
+                        <span className="calendar-input__title">
+                            Od kiedy wolne
+                        </span>
+                        <span>
+                            <img src={calendarIcon} alt="calendar icon" />
+                        </span>
+                        <input type="date"></input>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;

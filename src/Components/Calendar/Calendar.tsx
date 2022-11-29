1204b96da0e16b600e5a09fa28a471b6d2c8055d
NikodemCyrzan <nikodemcyrzan81@gmail.com>
2022-11-29 03:26:06 +0100

    feat: added calendar mechanics

--- src/Components/Calendar/Calendar.tsx
+++ src/Components/Calendar/Calendar.tsx
@@ -1,95 +1,176 @@
-import { useEffect, useRef, useState } from "react";
+import { ChangeEvent, useRef, useState } from "react";
 import calendarIcon from "./calendar.svg";
 import leftArrowIcon from "./left-arrow.svg";
 import rightArrowIcon from "./right-arrow.svg";
 
+type DropdownProps = {
+    visible: boolean;
+    date: { day: number; month: number; year: number };
+    monthBack: () => void;
+    monthForward: () => void;
+    setDate: (date: { day: number; month: number; year: number }) => void;
+};
+
 const Dropdown = ({
     visible = false,
     date,
-}: {
-    visible: boolean;
-    date: number;
-}) => {
+    monthBack,
+    monthForward,
+    setDate,
+}: DropdownProps) => {
+    const months = [
+        "Stryczeń",
+        "Luty",
+        "Marzec",
+        "Kwiecień",
+        "Maj",
+        "Czerwiec",
+        "Lipiec",
+        "Sierpień",
+        "Wrzesień",
+        "Październik",
+        "Listopad",
+        "Grudzień",
+    ];
+
+    date.day = date.day || 1;
+    date.month = date.month || 1;
+    date.year = date.year || 2020;
+
+    let startingDay = new Date(date.year, date.month - 1, 1).getDay(),
+        monthLength = new Date(date.year, date.month, 0).getDate();
+
+    startingDay = startingDay === 0 ? 7 : startingDay;
+
+    let header = ["Pn", "Wt", "Śr", "Cz", "Pt", "Sb", "Nd"].map((e) => (
+        <span key={e}>{e}</span>
+    ));
+    let days = Array(monthLength + startingDay - 1)
+        .fill(0)
+        .map((_, i) =>
+            i + 1 >= startingDay ? (
+                <span
+                    key={i}
+                    onClick={() =>
+                        setDate({
+                            day: i + 2 - startingDay,
+                            month: date.month,
+                            year: date.year,
+                        })
+                    }
+                    className={
+                        i + 2 - startingDay === date.day ? "focused" : ""
+                    }>
+                    {i + 2 - startingDay}
+                </span>
+            ) : (
+                <div key={i}></div>
+            )
+        );
+
     return (
         <div
             className="dropdown__container"
             style={{ visibility: visible ? "visible" : "hidden" }}>
             <div className="dropdown-header">
-                <img src={leftArrowIcon} alt="left arrow" />
-                <b>Maj 2022</b>
-                <img src={rightArrowIcon} alt="right arrow" />
+                <img src={leftArrowIcon} alt="left arrow" onClick={monthBack} />
+                <b>
+                    {months[date.month - 1]} {date.year}
+                </b>
+                <img
+                    src={rightArrowIcon}
+                    alt="right arrow"
+                    onClick={monthForward}
+                />
             </div>
             <div className="dropdown-grid">
-                <span>Pn</span>
-                <span>Wt</span>
-                <span>Śr</span>
-                <span>Cz</span>
-                <span>Pt</span>
-                <span>Sb</span>
-                <span>Nd</span>
-                <span>1</span>
-                <span>2</span>
-                <span>3</span>
-                <span>4</span>
-                <span>5</span>
-                <span>6</span>
-                <span>7</span>
-                <span>8</span>
-                <span>9</span>
-                <span>10</span>
-                <span>11</span>
-                <span>12</span>
-                <span>13</span>
-                <span>14</span>
-                <span>15</span>
-                <span>16</span>
-                <span>17</span>
-                <span>18</span>
-                <span>19</span>
-                <span>20</span>
-                <span>21</span>
-                <span>22</span>
-                <span>23</span>
-                <span>24</span>
-                <span>25</span>
-                <span>26</span>
-                <span>27</span>
-                <span>28</span>
-                <span>29</span>
-                <span>30</span>
-                <span>31</span>
+                {header}
+                {days}
             </div>
         </div>
     );
 };
 
 const Calendar = () => {
-    const [date, setDate] = useState<number>(0);
-    const [dropdownVisible, setDropdownVisibility] = useState(false);
+    const [date, setDate] = useState<{
+        day: number;
+        month: number;
+        year: number;
+    }>({ day: 0, month: 1, year: 2022 });
+    const [isDropdownVisible, setDropdownVisibility] = useState(false);
 
-    const inputEl = useRef<HTMLInputElement>(null),
-        calendarContainerEl = useRef<HTMLDivElement>(null);
+    const inputEl = useRef<HTMLInputElement>(null);
 
-    useEffect(() => {
-        console.log("Focus set");
-        inputEl.current?.addEventListener("change", () => {
-            setDate(Date.parse(inputEl.current?.value ?? "2022-01-01"));
-            console.log(Date.parse(inputEl.current?.value ?? "2022-01-01"));
+    const handleClick = () => {
+        setTimeout(() => {
+            if (inputEl.current !== document.activeElement)
+                inputEl.current?.focus();
+            if (!isDropdownVisible) setDropdownVisibility(true);
+        }, 1);
+    };
+
+    const formatYear = (year: number): number =>
+        Math.max(Math.min(year, 2030), 2020);
+    const formatMonth = (month: number): number =>
+        Math.max(Math.min(month, 12), 1);
+    const formatDay = (day: number, year: number, month: number): number =>
+        Math.max(Math.min(day, new Date(year, month, 0).getDate()), 1);
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const date = new Date(e.target.value),
+            year = formatYear(date.getFullYear()),
+            month = formatMonth(date.getMonth() + 1),
+            day = formatDay(date.getDate(), year, month);
+
+        console.log(e.target.value, year, month, day);
+
+        handleSetDate({
+            year,
+            month,
+            day,
         });
+    };
+    const handleMonthBack = () => {
+        const month = formatMonth(date.month - 1 <= 0 ? 12 : date.month - 1),
+            year = formatYear(date.month - 1 <= 0 ? date.year - 1 : date.year),
+            day = formatDay(date.day, year, month);
 
-        calendarContainerEl.current?.addEventListener("click", () => {
-            console.log("Focus click");
-            inputEl.current?.focus();
-            setDropdownVisibility(true);
+        setDate({
+            year,
+            month,
+            day,
         });
-    }, []);
+    };
+    const handleMonthforward = () => {
+        const month = formatMonth(date.month + 1 > 12 ? 1 : date.month + 1),
+            year = formatYear(date.month + 1 > 12 ? date.year + 1 : date.year),
+            day = formatDay(date.day, year, month);
+
+        setDate({
+            year,
+            month,
+            day,
+        });
+    };
+
+    const handleSetDate = (date: {
+        day: number;
+        month: number;
+        year: number;
+    }) => {
+        if (inputEl.current !== null)
+            inputEl.current.value = `${date.year}-${
+                date.month < 10 ? "0" + date.month : date.month
+            }-${date.day < 10 ? "0" + date.day : date.day}`;
+        setDate(date);
+    };
 
     return (
         <div className="calendar">
             <div className="calendar__wrapper">
                 <div className="calendar__container">
                     <div
-                        ref={calendarContainerEl}
+                        onClick={handleClick}
                         className="calendar-input__container">
                         <span className="calendar-input__title">
                             Od kiedy wolne
@@ -97,8 +178,17 @@ const Calendar = () => {
                         <span>
                             <img src={calendarIcon} alt="calendar icon" />
                         </span>
-                        <input ref={inputEl} type="date"></input>
-                        <Dropdown visible={dropdownVisible} date={date} />
+                        <input
+                            ref={inputEl}
+                            type="date"
+                            onInput={handleInputChange}></input>
+                        <Dropdown
+                            visible={isDropdownVisible}
+                            date={date}
+                            monthBack={handleMonthBack}
+                            monthForward={handleMonthforward}
+                            setDate={handleSetDate}
+                        />
                     </div>
                 </div>
             </div>
